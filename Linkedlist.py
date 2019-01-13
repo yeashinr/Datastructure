2c0f1fdd2fbf4d09f2932eea8be0c0548b045516
yeashinr <y_rabbi@live.concordia.ca>
2019-01-13 15:35:20 -0500

    create skeleton of Linkedlist

---
+++ Linkedlist.py
@@ -0,0 +1,84 @@
+class Node:
+
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Linkedlist:
+
+    def __init__(self):
+        self.head = None
+
+    def display_list(self):
+        pass
+    
+    def count_nodes(self):
+        pass
+    
+    def search(self,x):
+        pass
+    
+    def insert_at_head(self):
+        pass
+
+    def insert_at_tail(self):
+        pass
+    
+    def creat_list(self):
+        pass
+    
+    def insert_after(self):
+        pass
+    
+    def insert_befor(self):
+        pass
+    
+    def insert_at_position(self,position):
+        pass
+
+    def delete_node(self, position):
+        pass
+    
+    def delete_first_node(self):
+        pass
+
+    def delete_last_node(self):
+        pass
+    
+    def reverse_list(self):
+        pass
+
+    def bubble_sort_exdata(self):
+        pass
+
+    def bubble_sort_exlinks(self):
+        pass
+
+    def has_cycle(self):
+        pass
+
+    def find_cycle(self):
+        pass
+    
+    def remove_cycle(self):
+        pass
+    
+    def insert_cycle(self):
+        pass
+    
+    def merge2 (self,list):
+        pass
+    
+    def _merge2(self,p1,p2):
+        pass
+    
+    def merge_sort(self):
+        pass
+    
+    def _merge_sort_rec(self,listStart):
+        pass
+    
+    def _divide_list(self,p):
+        pass
+
+    
